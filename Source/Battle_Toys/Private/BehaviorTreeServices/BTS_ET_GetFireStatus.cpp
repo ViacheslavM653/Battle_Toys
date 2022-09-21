0d004a313f882f7bb463b0521e739be9f2b1de05
Viacheslav Martsynyshen <ganwolfworke@gmail.com>
2022-09-21 13:14:47 +0300

    Add "UBTS_ET_GetFireStatus"

---
+++ Source/Battle_Toys/Private/BehaviorTreeServices/BTS_ET_GetFireStatus.cpp
@@ -0,0 +1,42 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BehaviorTreeServices/BTS_ET_GetFireStatus.h"
+#include "GameFramework\Pawn.h"
+#include "BehaviorTree/BlackboardComponent.h"
+#include "AIController.h"
+#include "BasePawn/EnemyPawn/BaseEnemyTank/EnemyTank/EnemyTank.h"
+#include "Kismet/GameplayStatics.h"
+
+
+UBTS_ET_GetFireStatus::UBTS_ET_GetFireStatus()
+{
+	NodeName = TEXT("Get Fire Status");
+}
+
+void UBTS_ET_GetFireStatus::TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds)
+{
+    Super::TickNode(OwnerComp, NodeMemory, DeltaSeconds);
+
+    if (OwnerComp.GetAIOwner() == nullptr)
+    {
+        return;
+    }
+    APawn* OwnerPawn = OwnerComp.GetAIOwner()->GetPawn();
+    if (OwnerPawn == nullptr)
+    {
+        return;
+    }
+
+    if (Cast<AEnemyTank>(OwnerPawn))
+    {
+        bool FireStatus = Cast<AEnemyTank>(OwnerPawn)->IsReadyForFire();
+        
+        OwnerComp.GetBlackboardComponent()->SetValueAsBool(GetSelectedBlackboardKey(), FireStatus);
+
+    }
+
+
+}
+
+
